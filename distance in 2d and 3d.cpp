608f063500e1fe44541488550e740ff0701c0eaa
sanjanapanwar <54778729+sanjanapanwar@users.noreply.github.com>
2019-09-16 19:59:22 +0530

    Add files via upload

---
+++ distance in 2d and 3d.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+float distance(int x1,int x2,int y1,int y2)
+{
+    float d1;
+    d1=pow((pow((x1-x2),2)+pow((y1-y2),2)),0.5);
+    cout<<"the distance btw two points in 2d:"<<d1<<endl;
+}
+float distance(int x1,int x2,int y1,int y2,int z1,int z2)
+{
+    float d2;
+    d2=pow(pow((x2-x1),2)+pow((y2-y1),2)+pow((z2-z1),2),0.5);
+    cout<<"the distance btw two points in 3d :"<<d2<<endl;
+}
+
+int main()
+{
+   distance(2,4,6,8);
+   distance(2,4,6,8,5,1);
+
+}
